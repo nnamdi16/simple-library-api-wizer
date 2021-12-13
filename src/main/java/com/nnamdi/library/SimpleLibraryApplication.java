b725f72e2af4cf9be105e34fb9e65e6c10d387b6
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-12-13 12:06:09 +0100

    Spring boot project configuration and db set up

---
+++ src/main/java/com/nnamdi/library/SimpleLibraryApplication.java
@@ -0,0 +1,15 @@
+package com.nnamdi.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SimpleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibraryApplication.class, args);
+	}
+
+}
